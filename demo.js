ce549245572dafbb38e4b60d3ec7c6d26b07507f
sambit mahala <95913045+Mahala123@users.noreply.github.com>
2022-06-15 18:33:14 +0530

    Initial commit

---
+++ demo.js
@@ -0,0 +1,11 @@
+var items = document.getElementsByClassName("list-group-item");
+items[0].style.fontWeight = "bold";
+items[1].style.fontWeight = "bold";
+items[2].style.fontWeight = "bold";
+items[3].style.fontWeight = "bold";
+items[2].style.backgroundColor = "green";
+var items1 = document.getElementsByClassName("list-item1");
+items1[0].style.fontWeight = "bold";
+//var items1 = document.getElementsByTagName("li");
+//items[1].style.fontWeight = "bold";
+items1[0].style.backgroundColor = "green";
